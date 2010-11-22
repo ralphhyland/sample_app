8dd9368132e284d8e8c3adcb61fe87b3f5bb18a9
Ralph Hyland <ralph.hyland@gmail.com>
2010-11-22 21:10:55 +0000

    Finished Chap 5 Exercises

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,10 +4,14 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
 require 'spec/autorun'
 require 'spec/rails'
+require 'webrat'
 
 # Uncomment the next line to use webrat's matchers
 #require 'webrat/integrations/rspec-rails'
-
+Webrat.configure do |config|
+  config.mode = :rails
+end
+  
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
